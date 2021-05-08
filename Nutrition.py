490a72a6200fdb6354b150b12a7b85264dd112e2
humanharlan <55074932+humanharlan@users.noreply.github.com>
2021-05-07 23:00:35 -0700

    Update Nutrition.py

--- Nutrition.py
+++ Nutrition.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.optimize import nnls
+import csv
 
 nutrients = ["fat", "sodium", "carbs", "protein", "fiber", "vitD",
              "calcium", "iron", "potassium", "vitaminA", "vitaminC", "vitaminE",
@@ -16,17 +17,25 @@ targets = np.array([40, 25, 14, 15, 40, 21, 20, 20, 20, 20, 20, 20, 20, 20, 20, 
 def inputIngredient(ingredients, index):
     ingredient={} #create a dictionary for this ingredient
     print("~New Ingredient~\n")
-    ingredient["id"] = index
-    ingredient["name"] = input("Enter the name of the ingredient\n")
-    ingredient["unit"] = input("Enter the name of the units\n")
-    ingredient["serving"] = float(input("Enter the serving size\n"))
+    ingredient["ID"] = index
+    ingredient["Name"] = input("Enter the name of the ingredient\n")
+    ingredient["Unit"] = input("Enter the name of the units\n")
+    ingredient["Serving"] = float(input("Enter the serving size\n"))
     #Calorie input is divided by 10 to give it more balanced weighting with the other parameters
-    ingredient["calories"] = (float(input("Enter the number of calories\n"))/10)
+    ingredient["Calories"] = (float(input("Enter the number of calories\n"))/10)
 
     for nutrient in nutrients:
         ingredient[nutrient] = float(input("Enter the %DV of " + nutrient + "\n"))
 
     ingredients.append(ingredient)
+
+#reads a csv file and adds it to a list
+def readIngredient(ingredients, file):
+    with open(file) as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            row["Calories"] = float(row["Calories"])/10
+            ingredients.append(row)
 
 #get the data from the user. TODO: add automatic data extraction from a database
 
@@ -35,10 +44,13 @@ choice = "N"
 ingredientList = []
 
 #Loop to add ingredients until user chooses to calculate recipe
-while (choice != "C"):
-    inputIngredient(ingredientList, numberOfIngredients)
-    numberOfIngredients += 1
-    choice = input("\n(N)ew Ingredient\n(C)alculate Recipe\n")
+
+#while (choice != "C"):
+#    inputIngredient(ingredientList, numberOfIngredients)
+#    numberOfIngredients += 1
+#    choice = input("\n(N)ew Ingredient\n(C)alculate Recipe\n")
+
+readIngredient(ingredientList, "ingredients.csv")
 
 #Calculate a recipe
 
@@ -64,15 +76,13 @@ nutrientMatrix = np.array(values, dtype = 'float')
 #which makes rows=nutrient and columns=ingredient
 solution, residual = nnls(nutrientMatrix.T, targets)
 
-#TODO: Find how to constrain all solutions to be non-negative
-
 print("\nIngredients:\n")
 
 #print the amount of each ingredient greater than 0
 for i in range(0, len(solution)):
     if (solution[i]>0):
-        print("\n" + str((solution[i]*ingredientList[i]["serving"])) + " " +
-              str(ingredientList[i]["unit"]) + " of " + str(ingredientList[i]["name"]))
+        print("\n" + str((solution[i]*float(ingredientList[i]["Serving"]))) + " " +
+              str(ingredientList[i]["Unit"]) + " of " + str(ingredientList[i]["Name"]))
 
 #populate a list of nutrition facts for the recipe by summing the
 #nutrition facts of each ingredient multiplied by its quantity
@@ -80,13 +90,14 @@ recipeNutritionFacts = []
 for i in range(0,32):
     sum = 0
     for j in range (0, len(values)):
-        sum += (values[j][i] * solution[j])
+        sum += (float(values[j][i]) * solution[j])
     recipeNutritionFacts.append(sum)
     
 
 print("Residual (smaller number is closer to target nutrition): ")
 print(residual)
 
+#Calculate and print out the nutrition facts for the recipe
 print("\nNutrition Facts:")
 print("\nCalories: " + str(recipeNutritionFacts[0]*10))
 for i in range(1, 32):
